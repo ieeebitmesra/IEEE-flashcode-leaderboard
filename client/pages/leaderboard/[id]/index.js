1f89d22e690d974e37bb701e79cb060ad3f53d85
lohit244 <lohit244@gmail.com>
2022-10-11 23:46:02 +0530

    navigation links added

--- client/pages/leaderboard/[id]/index.js
+++ client/pages/leaderboard/[id]/index.js
@@ -1,14 +1,29 @@
 // yaha pe id specific data mangwana hai
 // uss data se leaderboard call krdenge
-
+import Link from "next/link";
 import Leaderboard from "../../../components/Leaderboard.js";
 
 export default function Home({ data }) {
   if (data.page != []) {
     // Data exists
+    const pages = [];
+    for (let i = 0; i < data.numberOfPages; i++) {
+      pages.push(i + 1);
+    }
     return (
-      <div className="px-4 md:px-12 pt-8">
+      <div className="sm:px-12 pt-8">
         <Leaderboard data={data} />
+        <div className="overflow-x-auto w-full flex justify-center mt-8">
+          {pages.map((page) => {
+            return (
+              <Link href={`/leaderboard/${page}`}>
+                <div className="cursor-pointer inline-block text-sm font-medium text-gray-900 p-2 sm:px-3 sm:py-2 md:px-6 md:py-4 bg-green-400 dark:bg-green-700/60 mx-2 rounded text-center dark:text-white">
+                  {page}
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     );
   }
